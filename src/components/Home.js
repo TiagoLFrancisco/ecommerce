6dbe02b84e4c52df8888a02e38f8fc749cee5293
Tiago Francisco <tiagolopesfrancisco@gmail.com>
2023-06-21 12:33:01 +0100

    Fetch data from fakestoreAPI and render a product list in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+
+      setProducts(response.data);
+    } catch (error) {
+      console.log("Error fetching products:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h3>{product.name}</h3>
+            <p>Title: {product.title}</p>
+            <p>Price: ${product.price}</p>
+            <p>Category: {product.category}</p>
+            <p>Description: {product.description}</p>
+            <p>
+              Image: <img src={product.image} alt={product.name} />
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
