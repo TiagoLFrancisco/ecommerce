c92e6b58b4d3c6d98ed481aeb043dc128d4f5a16
Tiago Francisco <tiagolopesfrancisco@gmail.com>
2023-06-23 11:04:34 +0100

    Add a Navbar between Home and Cart pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./components/Home";
 import ProductDetail from "./components/ProductDetail";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product_detail/:id" element={<ProductDetail />} />
-      </Routes>
+      <div>
+        <nav>
+          <ul style={{ display: "flex" }}>
+            <li style={{ marginRight: "40px" }}>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product_detail/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
