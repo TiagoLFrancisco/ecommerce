679877d4dd6f33f4d93f5a55fae67e33bf2ee6be
Tiago Francisco <tiagolopesfrancisco@gmail.com>
2023-06-26 11:55:15 +0100

    Update the ProductDetail list and buttons Use material-ui/react-list/ for the list Use material-ui/react-button/ for the button

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import List from "@mui/material/List";
+import { ListItem } from "@mui/material";
+import ListItemText from "@mui/material/ListItemText";
+import Button from "@mui/material/Button";
 
 function ProductDetail() {
   const location = useLocation();
@@ -36,38 +40,82 @@ function ProductDetail() {
     window.location.href = "/";
   };
 
+  const handleSelectedProductClick = () => {
+    //do open lightbox
+  };
+
   return (
     <div>
       <h2>Product Detail</h2>
+
       {selectedProduct && (
         <div>
-          <h3>{selectedProduct.title}</h3>
-          <p>Price: {selectedProduct.price} €</p>
-          <p>Description: {selectedProduct.description}</p>
-          <p>
-            <img
-              title={selectedProduct.title}
-              className="item-image"
-              src={selectedProduct.image}
-              alt={selectedProduct.name}
-            />
-          </p>
-          <button onClick={handleAddToCart}>Add to the cart</button>
-          <p>Related products:</p>
-          <p>
-            {filteredRelatedProducts.map((product) => (
-              <img
-                title={product.title}
-                key={product.id}
-                className="item-image"
-                src={product.image}
-                alt={product.name}
-                onClick={() => handleItemClick(product.id)}
+          <List>
+            <ListItem
+              key={selectedProduct.id}
+              onClick={() => handleSelectedProductClick}
+            >
+              <ListItemText
+                sx={{ marginRight: 10 }}
+                primary={selectedProduct.title}
+                secondary={
+                  <img
+                    title={selectedProduct.title}
+                    className="item-image"
+                    src={selectedProduct.image}
+                    alt={selectedProduct.name}
+                  />
+                }
               />
-            ))}
-          </p>
+              <ListItemText
+                sx={{ marginRight: 10 }}
+                primary={`Price:`}
+                secondary={`${selectedProduct.price} €`}
+              />
+              <ListItemText
+                sx={{ marginRight: 10 }}
+                primary={`Description:`}
+                secondary={selectedProduct.description}
+              />
+            </ListItem>
+          </List>
+          <div>
+            <Button onClick={handleAddToCart} variant="outlined">
+              Add to the cart
+            </Button>
+          </div>
 
-          <button onClick={handleGoBack}>Back to Item List</button>
+          <div>
+            <Button onClick={handleGoBack} variant="outlined">
+              Back to Products
+            </Button>
+          </div>
+
+          <h2>Related Products</h2>
+
+          <List>
+            {filteredRelatedProducts.map((product) => (
+              <ListItem
+                key={product.id}
+                onClick={() => handleItemClick(product.id)}
+              >
+                <ListItemText
+                  sx={{ marginRight: 10 }}
+                  primary={product.title}
+                  secondary={
+                    <img
+                      title={product.title}
+                      key={product.id}
+                      className="item-image"
+                      src={product.image}
+                      alt={product.name}
+                      onClick={() => handleItemClick(product.id)}
+                    />
+                  }
+                ></ListItemText>
+              </ListItem>
+            ))}
+          </List>
         </div>
       )}
     </div>
