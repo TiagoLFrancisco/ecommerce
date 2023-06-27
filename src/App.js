da2913aed215ac5d53898370bc2b16626935af02
Tiago Francisco <tiagolopesfrancisco@gmail.com>
2023-06-27 19:25:45 +0100

    Update breadcrumbs to buttons with icons Add color dark grey #424242 to them

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
 import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
-import Link from "@mui/material/Link";
-
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useState } from "react";
 import Home from "./components/Home";
 import ProductDetail from "./components/ProductDetail";
-
 import Cart from "./components/Cart";
 
 function App() {
   const randomId = Math.floor(Math.random() * 4) + 1;
+  const [value, setValue] = useState(0);
 
   return (
     <BrowserRouter>
       <div>
         <nav>
-          <Breadcrumbs aria-label="breadcrumb">
-            <Link underline="hover" color="inherit" href="/">
-              Home
-            </Link>
-
-            <Link
-              underline="hover"
-              color="inherit"
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => setValue(newValue)}
+            sx={{
+              justifyContent: "flex-start",
+              "& .Mui-selected": {
+                color: "dimgrey",
+              },
+            }}
+          >
+            <BottomNavigationAction
+              label="Home"
+              icon={<HomeIcon style={{ color: "#424242" }} />}
+              href="/"
+            />
+            <BottomNavigationAction
+              label="Shopping Basket"
+              icon={<ShoppingCartIcon style={{ color: "#424242" }} />}
               href={`/shopping_basket/${randomId}`}
-            >
-              Shopping Basket
-            </Link>
-          </Breadcrumbs>
+            />
+          </BottomNavigation>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product_detail/:id" element={<ProductDetail />} />
-
           <Route path="/shopping_basket/:id" element={<Cart />} />
         </Routes>
       </div>
